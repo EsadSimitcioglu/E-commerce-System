fd68466fb07d098aace20efb2bf355dd9297228f
Esat Simitçioğlu <esad.simitcioglu@ozu.edu.tr>
2022-04-15 12:15:17 +0300

    Add addBasket functionality

--- customerHomepage.js
+++ customerHomepage.js
@@ -1,42 +1,14 @@
+
+var productIds = [];
+
 function getFilterResearch() {
-    let name = document.getElementById("nameTextField").value; 
-    let surname = document.getElementById("surnameTextField").value; 
-    let email = document.getElementById("emailTextField").value; 
-    let password = document.getElementById("passwordTextField").value; 
-    let rePassword = document.getElementById("rePasswordTextField").value; 
-
-
-    if(password != rePassword){
-        alert("Your Passwords Doesn't Match. Try Again...")
-        return
-    }
-
-    if(document.getElementById("customerOption").checked){
-        axios.post('http://localhost:8080/customer', {
-        name: name, 
-        surname: surname, 
-        password : password,
-        email : email
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    }
-    else if(document.getElementById("storeOwnerOption").checked){
-        axios.post('http://localhost:8080/storeOwner', {
-        name: name, 
-        surname: surname, 
-        password : password,
-        email : email
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    }
+    
 }
+
+function addBasket(id){
+  productIds.push(id)
+  sessionStorage.setItem("productIds", JSON.stringify(productIds)); //store ids
+  console.log(productIds)
+}
+
+
---
+++ shoppingCart.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    var productIds = JSON.parse(sessionStorage.getItem("productIds")); //get them back
+    console.log(productIds)
+};
