6fe2b906ad2e55575bec38d011968435326144b6
Esat Simitçioğlu <esad.simitcioglu@ozu.edu.tr>
2022-04-27 16:47:43 +0300

    Add customer backend

--- customerHomepage.js
+++ customerHomepage.js
@@ -1,9 +1,26 @@
 
 var productIds = [];
 
+
+
 function getFilterResearch() {
     
 }
+
+function searchProduct() {
+  const keyword = document.getElementById("searchTextField").value 
+
+  axios.get(`http://localhost:8080/products/searchName/${keyword}`)
+      .then(function (response) {
+        for(var i = 0;i<response.length;i++){
+          productIds.push(response[i])
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+    });
+}
+
 
 function addBasket(id){
   productIds.push(id)
