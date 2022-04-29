2cd0cb89765242c13795b5c1a05deaf30912abd0
Chiostygi <govechio@gmail.com>
2022-04-29 08:36:28 +0300

    update

--- customerHomepage.js
+++ customerHomepage.js
@@ -15,9 +15,29 @@ window.onload = function() {
       .catch(function (error) {
         console.log(error);
     });
+
+    var allProducts = ""
+    axios.get(`http://localhost:8080/products`)
+      .then(function (response) {
+        for(var i = 0; i < response.data.length; i++) {
+          console.log(response.data[i].name)
+          console.log(response.data[i].price)
+          console.log(response.data[i].id)
+          allProducts += "<div class=\"col-lg-4 col-md-6 col-sm-6\" align=\"center\"><div class=\"product\" onclick=\"addBasket(" + response.data[i].id + "," + '\'' + response.data[i].name + '\'' + "," + response.data[i].price + ")\"><a><img class=\"img-fluid\" src=images\\unsplash_NOpsC3nWTzY.svg></a><div class=\"bottom\"><h4><img class=\"cart\" src=\"images\\Vector.svg\">$" + response.data[i].price + "</h4></div></div></div>"
+        }
+  
+        console.log(allProducts)
+        document.getElementById("products").innerHTML = allProducts
+      })
+      .catch(function (error) {
+        console.log(error);
+    })
+
+    
 };
 
 function addBasket(id,name,price){
+  console.log("Called")
   var obj = { "id": id, "name": name, "price" : price}
   orderList.push(obj)
   sessionStorage.setItem('orderList',JSON.stringify(orderList))
@@ -34,8 +54,10 @@ function searchProduct() {
       })
       .catch(function (error) {
         console.log(error);
-    });
+  });
 }
+
+
 
 function getDetailsOfProducts(id){
 
