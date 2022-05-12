98ce74a3d5920d39d42eea7ca2b5c5c7b06a8810
EsadSimitcioglu <esat-muhammet@hotmail.com>
2022-05-13 00:46:11 +0300

    Debug product-details

--- StoreOwner/SOProductManagement/SOProductManagement.js
+++ StoreOwner/SOProductManagement/SOProductManagement.js
@@ -16,6 +16,7 @@ window.onload = function () {
     axios.get(`http://localhost:8080/products/store/${storeID}`)
       .then(function (response) {
         for(var i = 0; i < response.data.length; i++) {
+          console.log(response.data)
           if(response.data[i].quantity == 0)
             continue
 
@@ -23,7 +24,7 @@ window.onload = function () {
           productMap.set(response.data[i].id,response.data[i].quantity)
           allProducts +=  "<div class='row' id='product-details'><div class='col-lg-2 col-md-2 col-sm-12 m-auto'> <div class='product'><img class='img-responsive' src='images\\unsplash_NOpsC3nWTzY.svg' alt='Product image'></div></div><div class='col-lg-2 col-md-2 col-sm-12 m-auto'><div class='product'><p class='text'>" + response.data[i].name + "</p></div></div><div class='col-lg-2 col-md-2 col-sm-12 m-auto'><div class='product' id='bottom-box-details'><p class='text'>" + response.data[i].description + "</p></div></div><div class='col-lg-2 col-md-2 col-sm-12 m-auto'><div class='row'><form><div class='value-button' id='decrease' onclick='decreaseValue(" + response.data[i].id + ")'value='Decrease Value'><i class='fa-solid fa-circle-minus'style='color:#161850'></i></div><input type='number' id=" + response.data[i].id + " value=" + response.data[i].quantity + " /><div class='value-button' id='increase' onclick='increaseValue(" + response.data[i].id + ")'value='Increase Value'><i class='fa-solid fa-circle-plus' style='color:#161850'></i></div></form></div></div><div class='col m-auto'><p class='text'>$ " + response.data[i].price + "</p></div></div>"
         }
-          document.getElementById("all-products").innerHTML = allProducts
+          document.getElementById("product-details").innerHTML = allProducts
           if(!window.location.hash) {
             window.location = window.location + '#loaded';
             window.location.reload();
